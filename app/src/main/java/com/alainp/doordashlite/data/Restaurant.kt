1843e702b80ea342cdf1016c9a855b5bb25ca566
Alain Pimentel <2334029+alainpimentel@users.noreply.github.com>
2021-03-09 13:59:18 -0800

    Using glide to load images

--- app/src/main/java/com/alainp/doordashlite/data/Restaurant.kt
+++ app/src/main/java/com/alainp/doordashlite/data/Restaurant.kt
@@ -6,4 +6,6 @@ data class Restaurant(
     val id: Int,
     val name: String,
     val description: String,
-    @SerializedName("distance_from_consumer") val distanceFromConsumer: Double)
+    @SerializedName("distance_from_consumer") val distanceFromConsumer: Double,
+    @SerializedName("cover_img_url") val coverImageUrl: String
+)
