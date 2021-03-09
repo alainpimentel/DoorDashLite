df134cb5a4fddbd1323ac5e182c7f6127d94d043
Alain Pimentel <2334029+alainpimentel@users.noreply.github.com>
2021-03-09 10:26:48 -0800

    Added a restaurant service to fetch data from API

--- app/src/main/java/com/alainp/doordashlite/data/Restaurant.kt
+++ app/src/main/java/com/alainp/doordashlite/data/Restaurant.kt
@@ -1,4 +1,9 @@
 package com.alainp.doordashlite.data
 
-data class Restaurant(val id: Int) {
-}
+import com.google.gson.annotations.SerializedName
+
+data class Restaurant(
+    val id: Int,
+    val name: String,
+    val description: String,
+    @SerializedName("distance_from_consumer") val distanceFromConsumer: Double)
--- app/src/main/java/com/alainp/doordashlite/viewmodels/RestaurantListViewModel.kt
+++ app/src/main/java/com/alainp/doordashlite/viewmodels/RestaurantListViewModel.kt
@@ -1,9 +1,10 @@
 package com.alainp.doordashlite.viewmodels
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.alainp.doordashlite.api.RestaurantService
+import com.alainp.doordashlite.data.GetRestaurantsResponse
 import com.alainp.doordashlite.data.Restaurant
 import kotlinx.coroutines.channels.ticker
 import kotlinx.coroutines.launch
@@ -17,22 +18,32 @@ class RestaurantListViewModel : ViewModel() {
     val tickerChannel = ticker(delayMillis = 5_000, initialDelayMillis = 0)
 
     init {
+        val service = RestaurantService.create()
+
         viewModelScope.launch {
             // TODO hook up to repository here
             //_restaurantList
 //            val restaurants: List<Restaurant> = (0..10).map { Restaurant(it) }
 //            _restaurantList.postValue(restaurants)
 
-            var count = 1
-            for (event in tickerChannel) {
-                val restaurants: List<Restaurant> = (0..count).map { Restaurant(it) }
-                _restaurantList.postValue(restaurants)
-                if (count++ == 10) {
-                    break
-                }
-            }
+            val restaurants: GetRestaurantsResponse = service.getRestaurants(
+                lat = 37.422740,
+                lng = -122.139956
+            )
 
-            tickerChannel.cancel()
+            _restaurantList.postValue(restaurants.stores)
+
+
+//            var count = 1
+//            for (event in tickerChannel) {
+//                val restaurants: List<Restaurant> = (0..count).map { Restaurant(it) }
+//                _restaurantList.postValue(restaurants)
+//                if (count++ == 10) {
+//                    break
+//                }
+//            }
+//
+//            tickerChannel.cancel()
         }
     }
 }
