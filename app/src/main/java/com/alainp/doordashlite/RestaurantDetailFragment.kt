9b8925c395e043541df6fb3ee8a7022ad75fafca
Alain Pimentel <2334029+alainpimentel@users.noreply.github.com>
2021-03-10 10:57:25 -0800

    Added restaurant detail fragment with very simple UI

--- app/src/main/java/com/alainp/doordashlite/RestaurantDetailFragment.kt
+++ app/src/main/java/com/alainp/doordashlite/RestaurantDetailFragment.kt
@@ -7,8 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.widget.NestedScrollView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.alainp.doordashlite.databinding.FragmentRestaurantDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -20,19 +24,44 @@ class RestaurantDetailFragment : Fragment() {
     private val args: RestaurantDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
         Log.d("messi", "Arg ${args.restaurantId}")
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val binding = DataBindingUtil.inflate<FragmentRestaurantDetailBinding>(
+            inflater,
+            R.layout.fragment_restaurant_detail,
+            container,
+            false
+        ).apply {
+            restaurantDetailScrollview.setOnScrollChangeListener(
+                NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+                    var isToolbarShown = false
+                    val shouldShowToolbar = scrollY > toolbar.height
+                    if (isToolbarShown != shouldShowToolbar) {
+                        isToolbarShown = shouldShowToolbar
+
+                        // Use shadow animator to add elevation if toolbar is shown
+                        appbar.isActivated = shouldShowToolbar
+
+                        // Show the plant name if toolbar is shown
+                        toolbarLayout.isTitleEnabled = shouldShowToolbar
+                    }
+                    toolbar.setNavigationOnClickListener { view ->
+                        view.findNavController().navigateUp()
+                    }
+                })
+
+        }
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+//
+//        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+//            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+//        }
     }
 }
